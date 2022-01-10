ad76e094b61900e6742a49e39a54139b92668ed5
Alexa Dominguez <alexa.dominguez@Alexas-MacBook-Pro.local>
2022-01-10 17:07:52 -0600

    Added packs hash to Depot class.

--- environment.rb
+++ environment.rb
@@ -1,5 +1,19 @@
 module Environment
     class Depot
+        attr_accessor :packs
+
+        def initialize
+            @packs = {
+                simple_transportation_pack: { intelligence: %i[cellphone], items: %i[medipack chevy_versa] },
+                standard_transportation_pack: { intelligence: %i[cellphone antenna], arsenal: %i[colt_1911],
+                                                items: %i[handcuffs medipack chemestry chevy_versa] },
+                simple_mission_pack: { intelligence: %i[infopack laptop cellphone antenna], arsenal: %i[colt_1911],
+                                       items: %i[handcuffs medipack chemistry financial] },
+                standard_mission_pack: { intelligence: %i[infopack laptop cellphone antenna], arsenal: %i[remington_870 colt_1911 machete hatchet],
+                                         items: %i[handcuffs medipack chemistry financial] }
+            }
+        end
+
     end
 
     class Control
