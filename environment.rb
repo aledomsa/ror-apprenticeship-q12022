de5daa8eea0518cbd07ed54037a46ae0688c95a2
Alexa Dominguez <alexa.dominguez@Alexas-MacBook-Pro.local>
2022-01-12 17:52:18 -0600

    Added Control class hash and new_mission method

--- environment.rb
+++ environment.rb
@@ -4,19 +4,52 @@ module Environment
 
         def initialize
             @packs = {
-                simple_transportation_pack: { intelligence: %i[cellphone], items: %i[medipack chevy_versa] },
-                standard_transportation_pack: { intelligence: %i[cellphone antenna], arsenal: %i[colt_1911],
-                                                items: %i[handcuffs medipack chemestry chevy_versa] },
-                simple_mission_pack: { intelligence: %i[infopack laptop cellphone antenna], arsenal: %i[colt_1911],
-                                       items: %i[handcuffs medipack chemistry financial] },
-                standard_mission_pack: { intelligence: %i[infopack laptop cellphone antenna], arsenal: %i[remington_870 colt_1911 machete hatchet],
-                                         items: %i[handcuffs medipack chemistry financial] }
+                simple_transportation_pack: { 
+                    intelligence: %i[cellphone], 
+                    items: %i[medipack chevy_versa] 
+                },
+                standard_transportation_pack: { 
+                    intelligence: %i[cellphone antenna], 
+                    arsenal: %i[colt_1911],
+                    items: %i[handcuffs medipack chemestry chevy_versa] 
+                },
+                simple_mission_pack: { 
+                    intelligence: %i[infopack laptop cellphone antenna], 
+                    arsenal: %i[colt_1911],
+                    items: %i[handcuffs medipack chemistry financial] 
+                },
+                standard_mission_pack: { 
+                    intelligence: %i[infopack laptop cellphone antenna], 
+                    arsenal: %i[remington_870 colt_1911 machete hatchet],
+                    items: %i[handcuffs medipack chemistry financial] 
+                }
             }
         end
 
     end
 
     class Control
+        attr_accessor :missions
+        
+        def initialize
+            @missions = {
+                name: {
+                    objective: "",
+                    pack: packs,
+                    status: active
+                }
+            }
+        end
+
+        def new_mission(name, objective, pack)
+            respond = {
+                missions.name: name,
+                missions.name.objective: objective,
+                missions.pack: pack
+            }
+            p respond
+        end
+
     end
 
     class Human
