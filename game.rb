ea8da18ad00fe67b23b3e70f39ce428dc951ba56
Alexa Dominguez <alexa.dominguez@Alexas-MacBook-Pro.local>
2022-01-14 14:05:12 -0600

    Added Class Game and initialize variables and hashes. Included Enviroment module and required environment file.

--- game.rb
+++ game.rb
@@ -1,1 +1,17 @@
 require 'environment.rb'
+
+class Game
+    include Environment
+    attr_accessor :board, :depot, :control
+
+    def initialize
+        @board = {
+            players: {},
+            control: {},
+            status: nil
+        }
+        @depot = Environment::Depot.new
+        @control = Environment::Control.new
+    end
+    
+end
