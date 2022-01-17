d822cda8f15fc2ff52b7e8682d0fcfe8d10caae0
Alexa Dominguez <alexa.dominguez@Alexas-MacBook-Pro.local>
2022-01-17 14:43:01 -0600

    Modify new_mission method to give ArgumentError text error

--- error.rb
+++ error.rb
@@ -2,4 +2,27 @@ require_relative 'environment.rb'
 
 class ErrorTest
     include Environment
+    attr_accessor :board, :depot, :control
+
+    def initialize
+        @board = {
+            players: {},
+            control: {},
+            status: nil
+        }
+        @depot = Environment::Depot.new
+        @control = Environment::Control.new
+    end
+
+    def new_worker name: nil
+        begin
+            @board[:players].store name, Environment::Worker.new(name)
+        rescue ArgumentError
+            puts 'No string for name in worker given'
+        end
+    end
+
 end
+
+error = ErrorTest.new
+error.new_worker
