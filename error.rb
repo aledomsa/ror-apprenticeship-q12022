cfc4bf0c2f54855987d2c8e307acba6be3e81401
Alexa Dominguez <alexa.dominguez@Alexas-MacBook-Pro.local>
2022-01-17 16:51:45 -0600

    Added 2 error handlings for new mission method

--- error.rb
+++ error.rb
@@ -15,14 +15,33 @@ class ErrorTest
     end
 
     def new_worker name: nil
-        begin
             @board[:players].store name, Environment::Worker.new(name)
         rescue ArgumentError
-            puts 'No string for name in worker given'
-        end
+            puts 'No string for name in worker method'
     end
 
+
+    def new_mission(name:, objective:, pack:)
+            @control.new_mission(name: name, objective: objectve, pack: pack)
+        rescue NameError
+            puts 'Undefined local variable in method'
+    end
+
+    def new_mission2(name:, objective:, pack:)
+            @control.new_mission(name: name, objective: objective)
+        rescue ArgumentError
+            puts 'Undefined argument in method'
+    end
 end
 
+
+
 error = ErrorTest.new
+
+name = :alpha
+objective = 'Get Alpha to the base'
+pack = :simple_transportation_pack
+
 error.new_worker
+error.new_mission(name: name, objective: objective, pack: pack)
+error.new_mission2(name: name, objective: objective, pack: pack)
