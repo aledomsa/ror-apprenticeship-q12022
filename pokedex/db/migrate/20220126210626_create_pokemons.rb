8b461e04a8b7bf9eac747767921fb6394c1b5ec7
Alexa Dominguez <alexa.dominguez@Alexas-MacBook-Pro.local>
2022-01-26 15:08:04 -0600

    Changed pokemon parameters of arrays for text

--- pokedex/db/migrate/20220126210626_create_pokemons.rb
+++ pokedex/db/migrate/20220126210626_create_pokemons.rb
@@ -1,14 +1,14 @@
 class CreatePokemons < ActiveRecord::Migration[6.1]
   def change
     create_table :pokemons do |t|
-      t.array :ability_list
+      t.text :ability_list
       t.integer :height
-      t.array :image_list
-      t.array :move_list
+      t.text :image_list
+      t.text :move_list
       t.string :name
       t.integer :pokemon_id
-      t.array :stat_list
-      t.array :type_list
+      t.text :stat_list
+      t.text :type_list
       t.string :url
       t.integer :weight
 
